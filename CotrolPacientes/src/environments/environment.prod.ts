295104af65081f8170c58f8c721609642049acaf
Machuca22 <jairo.mp.99@gmail.com>
2020-10-02 16:16:23 -0600

    Login al 100% utilizando firebase

---
+++ CotrolPacientes/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD1Hn1z9k7wLecngihWsrcZCIEOzA8_DB0",
+     authDomain: "clinicadps-a6e74.firebaseapp.com",
+     databaseURL: "https://clinicadps-a6e74.firebaseio.com",
+     projectId: "clinicadps-a6e74",
+     storageBucket: "clinicadps-a6e74.appspot.com",
+     messagingSenderId: "997581620912",
+     appId: "1:997581620912:web:b4d1852ed0fb4096335d75",
+     measurementId: "G-D407CG4FNC"
+  }
+};
---
+++ CotrolPacientes/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD1Hn1z9k7wLecngihWsrcZCIEOzA8_DB0",
+     authDomain: "clinicadps-a6e74.firebaseapp.com",
+     databaseURL: "https://clinicadps-a6e74.firebaseio.com",
+     projectId: "clinicadps-a6e74",
+     storageBucket: "clinicadps-a6e74.appspot.com",
+     messagingSenderId: "997581620912",
+     appId: "1:997581620912:web:b4d1852ed0fb4096335d75",
+     measurementId: "G-D407CG4FNC"
+  }
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
